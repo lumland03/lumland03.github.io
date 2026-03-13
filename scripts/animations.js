2e841771d4afce779bc8230391fb44ba60f65786
Landon Umland <lumland@ewu.edu>
2026-03-13 05:00:18 +0000

    added parallax functionality

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,8 +1,42 @@
-import { gsap } from "gsap";
+import gsap from "https://esm.sh/gsap";
+import ScrollTrigger from "https://esm.sh/gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+
+
+gsap.to(".layer-bg", {
+  y: -100,
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".parallax-scene",
+    start: "top bottom",
+    end: "bottom top",
+    scrub: true,
+    markers: true,
+   
+  }
+});
+gsap.to('.layer-mid', {
+  y: 50,
+  ease: "none",
+  scrollTrigger: {
+    trigger:".parallax-scene", 
+    start: "top bottom",
+    end: "bottom top",
+    scrub: true,
+    markers: true,
+
+}
+});
+gsap.to('.layer-fg', {
+  y: 200,
+  ease: "none",
+  scrollTrigger: {
+    trigger:".parallax-scene", 
+    start: "top bottom",
+    end: "bottom top",
+    scrub: true,
+    markers: true,
     
-import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(DrawSVGPlugin,ScrollTrigger);
-
-
+}
+});
