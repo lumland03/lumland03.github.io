31f9ebea1eae0954f2a9ae479b129e78d0228546
Landon Umland <lumland@ewu.edu>
2026-03-13 04:14:56 +0000

    setup

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+## My Digital Garden
---
+++ scripts/animations.js
@@ -0,0 +1,8 @@
+import { gsap } from "gsap";
+    
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(DrawSVGPlugin,ScrollTrigger);
+
+
